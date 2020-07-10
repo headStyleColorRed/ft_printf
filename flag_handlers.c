807713d83ac6e55f377fd9cebaf099403dc49bcd
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-07-10 23:27:29 +0200

    flag_handlers passes norminette

--- flag_handlers.c
+++ flag_handlers.c
@@ -6,13 +6,13 @@
 /*   By: rlabrado <headstylecolorred@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 23:12:06 by rlabrado          #+#    #+#             */
-/*   Updated: 2020/07/10 23:13:08 by rlabrado         ###   ########.fr       */
+/*   Updated: 2020/07/10 23:26:51 by rlabrado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf_lib.h"
 
-int flag_one_handler()
+int		flag_one_handler(void)
 {
 	int spaces_to_write;
 	int zeros_to_write;
@@ -23,7 +23,6 @@ int flag_one_handler()
 	zeros_to_write = get_modifier(type_precision);
 	written_result = 0;
 	result = 0;
-
 	if (zeros_to_write > get_wordifier(type_w_length))
 	{
 		if (get_wordifier(type_is_negative))
@@ -39,35 +38,7 @@ int flag_one_handler()
 	return (result);
 }
 
-int flag_two_handler()
-{
-		int spaces_to_write;
-	int zeros_to_write;
-	int written_result;
-	int result;
-
-	spaces_to_write = get_modifier(type_width);
-	zeros_to_write = get_modifier(type_precision);
-	written_result = 0;
-	result = 0;
-
-		zeros_to_write = get_modifier(type_precision) - get_wordifier(type_w_length);
-		if (get_wordifier(type_is_negative))
-		{
-			write(1, "-", 1);
-			zeros_to_write++;
-			set_wordifier(type_is_negative, 0);
-		}
-		while (zeros_to_write-- > 0)
-		{
-			write(1, "0", 1);
-			result++;
-		}
-	return (result);
-}
-
-
-int handler_zero_flag()
+int		flag_two_handler(void)
 {
 	int spaces_to_write;
 	int zeros_to_write;
@@ -78,18 +49,49 @@ int handler_zero_flag()
 	zeros_to_write = get_modifier(type_precision);
 	written_result = 0;
 	result = 0;
+	zeros_to_write = get_modifier(type_precision) -
+	get_wordifier(type_w_length);
+	if (get_wordifier(type_is_negative))
+	{
+		write(1, "-", 1);
+		zeros_to_write++;
+		set_wordifier(type_is_negative, 0);
+	}
+	while (zeros_to_write-- > 0)
+	{
+		write(1, "0", 1);
+		result++;
+	}
+	return (result);
+}
+
+void	handle_negative_number(void)
+{
+	if (get_wordifier(type_is_negative))
+	{
+		write(1, "-", 1);
+		set_wordifier(type_is_negative, 0);
+	}
+}
+
+int		handler_zero_flag(void)
+{
+	int spaces_to_write;
+	int zeros_to_write;
+	int result;
+
+	spaces_to_write = get_modifier(type_width);
+	zeros_to_write = get_modifier(type_precision);
+	result = 0;
 	if (spaces_to_write > zeros_to_write)
 		result += flag_one_handler();
 	if (get_modifier(type_precision) != 1111)
 		result += flag_two_handler();
 	else
 	{
-		zeros_to_write = get_modifier(type_width) - get_wordifier(type_w_length);
-		if (get_wordifier(type_is_negative))
-		{
-			write(1, "-", 1);
-			set_wordifier(type_is_negative, 0);
-		}
+		zeros_to_write = get_modifier(type_width) -
+		get_wordifier(type_w_length);
+		handle_negative_number();
 		while (zeros_to_write-- > 0)
 		{
 			write(1, "0", 1);
