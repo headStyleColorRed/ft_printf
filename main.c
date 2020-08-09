3861a188279c3452299e9f7f46eaa08b1ff48f85
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-08-09 19:18:55 +0200

    fixed c --

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include "printf_lib.h"
 #include <limits.h>
+#include <assert.h>
 
 int main(void)
 {
-	int my_total;
-	// int his_total;
+	char c = 'y';
 
-	char *string = "Lesenfantsnesententpastrestresbonmaiscac'estparcequ'ilsfontcacadansleurculotteetquecen'estvraimentpastrestrespropreNonmongroscoupdecœurc'étaitunsoirde2005,j'étaisinvitéàuneconférenceàLaSorbonnepourparlerdelachansonfrancaiseetdel'écrituredetexteavecpleinsd'autresgens.ParmilesstarsémergentesilyavaitOliviaRuizquietaitentraindedevenirunestaravecsachansonsurlechocolatàlacon.Brefjeelretrouvecoincélorsd'une\"atelierderéflexion\"entreOliviaetRichardCrossquicommençaitàêtrepasmalconnudanslemilieuencemomentàcausedequelquesémissionsdeteréalité.J'aibienrigoleavecOliviaRuizcesoirlà,jeluiparlaitdemonrêved'écritetunechansondepirate,elledelafaçonqu'elleavaitd'écriredeschansons\"commeellessortaient\"etdes'étonnerparfoisqued'autrestrouventcabien.Brefçac'étaitunechouettesoirée/nuitquis'estterminéeauxalentoursde7hdumatinenprenantuncaféauChaidel'AbbayeprèsdeMabillon.Avantqu'ellenedisparaissepourtoujoursdemonexistence.JesuissouventrepasséauChaitôtlematinenespérantlarevoirmaisniet.Rien.JusteChristineScottThomasaperçuunjeudimatinoùilfaisaitchaud.Lesenfantsnesententpastrestresbonmaiscac'estparcequ'ilsfontcacadansleurculotteetquecen'estvraimentpastrestrespropreNonmongroscoupdecœurc'étaitunsoirde2005,j'étaisinvitéàuneconférenceàLaSorbonnepourparlerdelachansonfrancaiseetdel'écrituredetexteavecpleinsd'autresgens.";
+	assert(printf("-->|%--4.c|<--\n", c) == ft_printf("-->|%--4.c|<--\n", c));
+	assert(printf("-->|%--3.c|<--\n", c) == ft_printf("-->|%--3.c|<--\n", c));
+	assert(printf("-->|%--3c|<--\n", c) == ft_printf("-->|%--3c|<--\n", c));
+	assert(printf("-->|%--2.c|<--\n", c) == ft_printf("-->|%--2.c|<--\n", c));
+	assert(printf("-->|%--2c|<--\n", c) == ft_printf("-->|%--2c|<--\n", c));
+	assert(printf("-->|%--1.c|<--\n", c) == ft_printf("-->|%--1.c|<--\n", c));
+	assert(printf("-->|%--1c|<--\n", c) == ft_printf("-->|%--1c|<--\n", c));
 
-	my_total = ft_printf("%s\n", string);
-	// his_total = printf("%s\n", string);
+		int my_total;
+	int his_total;
+
+	my_total = ft_printf("-->|%--4.c|<--\n", c);
+	his_total = printf("-->|%--4.c|<--\n", c);
 	printf("My  total -> %d\n", my_total);
-	// printf("His total -> %d\n\n", his_total);
-	
+	printf("His total -> %d\n\n", his_total);
 }
