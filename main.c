07002356c20b9133d9c05177732b3f4a57a7a442
Rodrigo Labrador <headstylecolorred@gmail.com>
2020-08-09 12:37:05 +0200

    corrected pointer issues

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
 int main(void)
 {
 	int my_total;
-	// int his_total;
+	int his_total;
 
-	// my_total = ft_printf("%.5p", 0);
-	my_total = ft_printf("%.p", NULL);
-	// his_total = printf("%0-3.7u", 3267);
-	// printf("My  total -> %d\n", my_total);
-	// printf("His total -> %d\n\n", his_total);
+	my_total = ft_printf("%-*.*s\n", -7, -3, "yolo");
+	his_total = printf("%-*.*s\n", -7, -3, "yolo");
+	printf("My  total -> %d\n", my_total);
+	printf("His total -> %d\n\n", his_total);
 	
 }
